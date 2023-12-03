ee98a804b3504858bd0404841fe893690e5d7434
Il Harper <hi@ilharper.com>
2023-12-03 23:45:58 +0800

    feat: add code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,148 @@
-import { Context, Schema } from 'koishi'
+import type { Context } from 'koishi'
+import { Schema, h } from 'koishi'
 
 export const name = 'memes'
 
-export interface Config {}
+export interface Config {
+  name: string
+  endpoint: string
+  memes: {
+    key: string
+    name: string
+  }[]
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  name: Schema.string().required(),
+  endpoint: Schema.string().default('http://127.0.0.1:2233'),
+  memes: Schema.array(
+    Schema.object({
+      key: Schema.string(),
+      name: Schema.string(),
+    }),
+  )
+    .default([])
+    .role('table'),
+})
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export interface MemeInfo {
+  key: string
+  keywords: string[]
+  patterns: unknown[]
+  params: {
+    min_images: number
+    max_images: number
+    min_texts: number
+    max_texts: number
+    default_texts: string[]
+    args: {
+      name: string
+      type: string
+      description: string
+      default: string
+    }[]
+  }
+}
+
+export async function apply(ctx: Context, config: Config) {
+  if (!config.memes?.length) return
+
+  const l = ctx.logger('m6c/memes')
+
+  if (config.endpoint.endsWith('/'))
+    config.endpoint = config.endpoint.slice(0, config.endpoint.length - 1)
+
+  for (const item of config.memes) {
+    const { key: meme, name: memeName } = item
+    try {
+      const info: MemeInfo = await ctx.http.get(
+        `${config.endpoint}/memes/${meme}/info`,
+      )
+
+      const cmdName = `memegen-${meme.toLowerCase().replaceAll('_', '-')}`
+
+      const cmd = ctx.command(
+        `${cmdName} ${[...Array(info.params.min_images).keys()]
+          .map((x) => `<image${x}:string>`)
+          .join(' ')} ${[...Array(info.params.min_texts).keys()]
+          .map((x) => `<text${x}:string>`)
+          .join(' ')} ${[
+          ...Array(info.params.max_images - info.params.min_images).keys(),
+        ]
+          .map((x) => `[image${x}:string]`)
+          .join(' ')} ${[
+          ...Array(info.params.max_texts - info.params.min_texts).keys(),
+        ]
+          .map((x) => `[text${x}:string]`)
+          .join(' ')}`,
+        `生成${memeName}图片`,
+      )
+
+      if (meme !== memeName) cmd.alias(memeName)
+
+      const example = `@${config.name} /${memeName} ${info.params.default_texts
+        .map((x) => `"${x}"`)
+        .join(' ')}`
+
+      cmd.example(example)
+
+      for (const arg of info.params.args)
+        cmd.option(arg.name, `[value:string] ${arg.description}`, {
+          fallback: undefined,
+        })
+
+      cmd.action(async ({ session, options }, ...args) => {
+        try {
+          if (!args.length) {
+            // 未提供任何参数，输出示例图片
+            const result = await ctx.http.axios({
+              url: `${config.endpoint}/memes/${meme}/preview`,
+              method: 'GET',
+              responseType: 'arraybuffer',
+            })
+
+            await session.send(
+              h.image(
+                result.data as Buffer,
+                result.headers['Content-Type'] as string,
+              ),
+            )
+
+            // 图片输出后，显示帮助
+            return session.execute(`help ${cmdName}`)
+          }
+
+          // TODO：将图片参数解析为图片
+
+          const data = new FormData()
+          args.forEach((x) => data.append('texts', x))
+          if (Object.entries(options).filter((x) => x[1]).length)
+            data.append('args', JSON.stringify(options))
+
+          const result = await ctx.http.axios({
+            url: `${config.endpoint}/memes/${meme}`,
+            method: 'POST',
+            data,
+            responseType: 'arraybuffer',
+          })
+
+          return h.image(
+            result.data as Buffer,
+            result.headers['Content-Type'] as string,
+          )
+        } catch (e) {
+          l.warn(e)
+          void session.send(
+            `生成图片失败，请检查输入格式哦~使用示例：\n${example}`,
+          )
+        }
+      })
+    } catch (e) {
+      l.warn(`Meme ${meme} load failed:`)
+      l.warn(e)
+      return
+    }
+  }
+
+  l.success(`${config.memes.length} meme(s) loaded.`)
 }
